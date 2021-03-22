7f1dcd93eef75a5828a07149ceb3d42326dcb846
Felix F Xu <fei4xu@gmail.com>
2021-03-23 06:07:41 +0800

    fix SKIA_INCLUDE_DIR

--- SkiaConfig.cmake
+++ SkiaConfig.cmake
@@ -13,13 +13,10 @@ Once done this will define:
 
 set(SKIA_FOUND 0)
 
-#/usr/local/include/skia
-find_path(SKIA_INCLUDE_DIR include/c/sk_canvas.h)
+find_path(SKIA_ROOT_INCLUDE_DIR Skia/include/c/sk_canvas.h)
+set(SKIA_INCLUDE_DIR ${SKIA_ROOT_INCLUDE_DIR}/Skia) #/usr/local/include/Skia
 
-#/usr/local/lib
-find_library(SKIA_LIBRARIES skia)
-
-message(STATUS ${SKIA_LIBRARIES})
+find_library(SKIA_LIBRARIES skia)                   #/usr/local/lib
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Skia DEFAULT_MSG SKIA_LIBRARIES SKIA_INCLUDE_DIR)
